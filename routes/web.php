22264bd203347f4d2a34e1ffdc4b53824ab404f9
skmercur <khoudoursofiane75@gmail.com>
2018-12-25 21:04:45 +0100

    Admin logins

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,19 @@ Route::get('/test', function () {
     return view('test');
 });
 
-Auth::routes();
+$this->get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('admin/login', 'Auth\LoginController@login');
+$this->post('admin/logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('admin/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('admin/register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('admin/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+ $this->post('admin/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('admin/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('admin/password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', function(){
     return view('admin');
